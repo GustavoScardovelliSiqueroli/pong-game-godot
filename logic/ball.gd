828a2949a01618afbcc89110debd32866b21c10b
Gustavo Scardovelli <gustavosecardovelli@gmail.com>
2024-11-14 16:45:37 -0300

    init: start a project with modifications in game pong

---
+++ logic/ball.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+const DEFAULT_SPEED = 100
+
+var _speed = DEFAULT_SPEED
+var direction = Vector2.LEFT
+@onready var _initial_pos = position
+
+
+
+func _process(delta):
+	# delta e em torno de 0.016;
+	
+	# direction comeca com (-1,0);
+	
+	# direction e definida com -1 ou 1 para x e um valor aleatorio
+	# entre 1 e -1, depois disso realiza a funcao normilized
+	# que mantera a distancia entre x e y em 1. isso acontece
+	# pq se o x fosse fixo, o valor aleatorio do y iria impactar
+	# na velocidade da bola;
+	
+	_speed += delta * 2
+	# speed comeca com 100 e e somada com um restinho do delta vezes 2
+	# para ir aumentando aos poucos (ela poderia ser multiplicado po 0.3
+	# que daria na mesma;
+	
+	# direction multiplicada com speed e multiplicada
+	# novamnete por delta para diminuir o speed e 
+	# nao somar muitas casas na position;
+	position += _speed * delta * direction
+
+
+func reset():
+	direction = Vector2.LEFT
+	position = _initial_pos
+	_speed = DEFAULT_SPEED
